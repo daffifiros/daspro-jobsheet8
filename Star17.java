14ef5f3f75164559b3b84c74e24bc30ff560dc88
daffifiros <daffikun10@gmail.com>
2024-10-23 11:30:26 +0700

    Hasil Output

--- Star17.java
+++ Star17.java
@@ -1,10 +1,40 @@
+// import java.util.Scanner;
+// public class Star17 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Masukkan Nilai N = ");
+//         int N = sc.nextInt();
+//         for (int i = 1; i <=N; i++) {
+//         System.out.print("*");
+
+//         }
+
+
+
+//     }
+// }
+// import java.util.Scanner;
+// public class Star17 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Masukkan Nilai N = ");
+//         int N = sc.nextInt();
+//         for (int i = 0; i <=N; i++) {
+//         System.out.print("*");
+
+//         }
+
+
+
+//     }
+// }
 import java.util.Scanner;
 public class Star17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan Nilai N = ");
         int N = sc.nextInt();
-        for (int i = 1; i <=N; i++) {
+        for (int i = 0; i >N; i++) {
         System.out.print("*");
 
         }
